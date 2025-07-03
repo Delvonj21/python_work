855eaf3406064a9d52c5ab4b9fb78a21b1a97b07
Delvon <delvonj21@gmail.com>
2025-07-03 18:07:07 +0200

    added files

---
+++ Lists/names.py
@@ -0,0 +1,5 @@
+names = ["Jeff", "Marcus", "Jemal"]
+
+print(names[0])
+print(names[1])
+print(names[2])
---
+++ Lists/your_own_list.py
@@ -0,0 +1,13 @@
+cars = ["Range", "Honda", "Toyota", "Chevy"]
+
+msg = f"My first car was a {cars[2].title()}."
+print(msg)
+
+msg = f"My dream car is a {cars[0].title()}."
+print(msg)
+
+msg = f"My wife now drives a {cars[1].title()}."
+print(msg)
+
+msg = f"My best friend owns a {cars[3].title()}."
+print(msg)
