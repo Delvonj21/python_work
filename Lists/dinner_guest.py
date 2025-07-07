b16cfdb31ebedd424786acd6a07a82017e9aa781
Delvon <delvonj21@gmail.com>
2025-07-07 15:57:10 +0200

    added files

---
+++ Lists/dinner_guest.py
@@ -0,0 +1,14 @@
+# Invite some people to dinner
+guests = ["Date Mike", "Prison Mike", "Michael Scarn"]
+
+name = guests[0].title()
+print(f"{name}, please come to dinner.")
+
+name = guests[1].title()
+print(f"{name}, please come to dinner.")
+
+name = guests[2].title()
+print(f"{name}, please come to dinner.")
+
+number_of_people = len(guests)
+print(f"I have {number_of_people} coming to dinner.")
