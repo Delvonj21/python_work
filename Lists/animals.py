49b715c88e27916a51186ec600f3e54966ed1654
Delvon <delvonj21@gmail.com>
2025-07-08 18:10:05 +0200

    added files

---
+++ Lists/animals.py
@@ -0,0 +1,14 @@
+animals = ["loin", "tiger", "bear"]
+
+# Print each animal
+for animal in animals:
+    print(animal)
+
+print("\n")
+
+# Print a statement about each animal
+for animal in animals:
+    print(f"A {animal} has 4 legs.")
+
+
+print("\nAll of these animals have 4 legs. ")
---
+++ Lists/pizzas.py
@@ -0,0 +1,13 @@
+pizzas = ["pepperoni", "sausage", "spinach and chicken"]
+
+# Print the names of all the pizzas
+for pizza in pizzas:
+    print(pizza)
+
+print("\n")
+
+# Print a sentence about each pizza
+for pizza in pizzas:
+    print(f"I really like {pizza} pizza!")
+
+print("\nI really love pizza!")
