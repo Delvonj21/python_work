40de8fa795355e37fb3d6fe53eaa217dbc43c1f9
Delvon <delvonj21@gmail.com>
2025-07-11 19:38:16 +0200

    added files

---
+++ Lists/buffet.py
@@ -0,0 +1,25 @@
+menu_items = (
+    "rockfish",
+    "halibut nuggets",
+    "crab cakes",
+    "salmon burger",
+    "smoked chowder",
+)
+
+print("You can choose from the following menu items:")
+for item in menu_items:
+    print(f"- {item}")
+
+menu_items = (
+    "rockfish",
+    "halibut nuggets",
+    "crab cakes",
+    "salmon burger",
+    "black cod tips",
+    "king crab legs",
+)
+
+print("\nOur menu has been updated.")
+print("You can now choose from the following items:")
+for item in menu_items:
+    print(f"- {item}")
